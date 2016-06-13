46e22a4492467949465ac99b48659e6cf4b37e0c
hundvalp <hundvalp@users.noreply.github.com>
2016-06-13 18:33:21 -0500

    Updated main code to reflect new interface

--- main.py
+++ main.py
@@ -1,42 +1,43 @@
 from pyglet.gl import glEnable, glBlendFunc, GL_BLEND, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA
-from pyglet.sprite import Sprite
+from pyglet import resource
+from pyglet import window
+from pyglet import clock
+from pyglet import app
 from game_object import GameObject
 from game_button import GameButton
+from basic_graphic import BasicGraphic
 from basic_animation import BasicAnimation
-import pyglet
 
 frame_rate = 120
 
-pyglet.resource.path = ['resources']
-pyglet.resource.reindex()
+resource.path = ['resources']
+resource.reindex()
 
-donut       = GameObject(pyglet.resource.image("donut.png"),       x=400, y=300)
-donut_hover = GameObject(pyglet.resource.image("donut-hover.png"), x=400, y=300)
+donut       = GameObject(BasicGraphic.from_image("donut.png"),       x=400, y=300)
+donut_hover = GameObject(BasicGraphic.from_image("donut-hover.png"), x=400, y=300)
 
 ani_donut_frames = []
 ani_donut_durations = [1.0/24 for x in range(40)]
 for x in range(1, 40+1):
-	ani_donut_frames.append(
-		pyglet.resource.image('pink-trans-donut/trans-donut{0:04d}.png'.format(x)))
+	ani_donut_frames.append('pink-trans-donut/trans-donut{0:04d}.png'.format(x))
 
-ani_donut_animation = BasicAnimation.from_image_sequence(
+ani_donut_animation = BasicAnimation.from_images(
 	ani_donut_frames, ani_donut_durations, loops=True)
 
 ani_donut_hover_frames = []
 for x in range(1, 40+1):
-	ani_donut_hover_frames.append(
-		pyglet.resource.image('blue-trans-donut/trans-donut{0:04d}.png'.format(x)))
+	ani_donut_hover_frames.append('blue-trans-donut/trans-donut{0:04d}.png'.format(x))
 
-ani_donut_hover_animation = BasicAnimation.from_image_sequence(
+ani_donut_hover_animation = BasicAnimation.from_images(
 	ani_donut_hover_frames, ani_donut_durations, loops=True)
 
 ani_donut       = GameObject(ani_donut_animation,       x=100, y=100)
 ani_donut_hover = GameObject(ani_donut_hover_animation, x=100, y=100)
 
-game_window = pyglet.window.Window(800, 600, caption="Pet Cafe")
+game_window = window.Window(800, 600, caption="Pet Cafe")
 #game_window.set_icon(pyglet.resource.image('icon.png')) # TODO Load an icon
 
-key_handler = pyglet.window.key.KeyStateHandler()
+key_handler = window.key.KeyStateHandler()
 game_window.push_handlers(key_handler)
 
 # TODO Don't hardcode this
@@ -66,5 +67,5 @@ if __name__ == "__main__":
 	glEnable(GL_BLEND)
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
-	pyglet.clock.schedule_interval(update, 1.0/frame_rate)
-	pyglet.app.run()
+	clock.schedule_interval(update, 1.0/frame_rate)
+	app.run()
