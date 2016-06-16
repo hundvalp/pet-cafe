9e3a139ca6c9d9284dbae9804132fbf865c22f31
hundvalp <hundvalp@users.noreply.github.com>
2016-06-16 16:33:34 -0500

    Removed unnecessary classes, use Pyglet classes

--- main.py
+++ main.py
@@ -1,41 +1,14 @@
 from pyglet.gl import glEnable, glBlendFunc, GL_BLEND, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA
-from pyglet import resource
-from pyglet import window
-from pyglet import clock
-from pyglet import app
-from game_object import GameObject
+from pyglet.image import Animation
+from pyglet.sprite import Sprite
+from pyglet import resource, window, clock, app
 from game_button import GameButton
-from game_object_silhouette import GameObjectSilhouette
-from basic_graphic import BasicGraphic
-from basic_animation import BasicAnimation
+from sprite_silhouette import SpriteSilhouette
 
 frame_rate = 120
 
 resource.path = ['resources']
 resource.reindex()
-
-donut       = GameObject(BasicGraphic.from_image("donut.png"),       x=400, y=300)
-donut_hover = GameObject(BasicGraphic.from_image("donut-hover.png"), x=400, y=300)
-
-donut_silhouette = GameObjectSilhouette(donut, (255,255,150), x=500, y=100)
-
-ani_donut_frames = []
-ani_donut_durations = [1.0/24 for x in range(40)]
-for x in range(1, 40+1):
-	ani_donut_frames.append('pink-trans-donut/trans-donut{0:04d}.png'.format(x))
-
-ani_donut_animation = BasicAnimation.from_images(
-	ani_donut_frames, ani_donut_durations, loops=True)
-
-ani_donut_hover_frames = []
-for x in range(1, 40+1):
-	ani_donut_hover_frames.append('blue-trans-donut/trans-donut{0:04d}.png'.format(x))
-
-ani_donut_hover_animation = BasicAnimation.from_images(
-	ani_donut_hover_frames, ani_donut_durations, loops=True)
-
-ani_donut       = GameObject(ani_donut_animation,       x=100, y=100)
-ani_donut_hover = GameObject(ani_donut_hover_animation, x=100, y=100)
 
 game_window = window.Window(800, 600, caption="Pet Cafe")
 #game_window.set_icon(pyglet.resource.image('icon.png')) # TODO Load an icon
@@ -44,8 +17,32 @@ key_handler = window.key.KeyStateHandler()
 game_window.push_handlers(key_handler)
 
 # TODO Don't hardcode this
+donut       = Sprite(resource.image("donut.png"),       x=400, y=300)
+donut_hover = Sprite(resource.image("donut-hover.png"), x=400, y=300)
 button = GameButton(donut, hover=donut_hover)
+
+# TODO Don't hardcode this
+ani_donut_frames = []
+for x in range(1, 40+1):
+	ani_donut_frames.append(resource.image('pink-trans-donut/trans-donut{0:04d}.png'.format(x)))
+
+ani_donut_animation = Animation.from_image_sequence(
+	ani_donut_frames, 1.0/24, loop=True)
+
+ani_donut_hover_frames = []
+for x in range(1, 40+1):
+	ani_donut_hover_frames.append(resource.image('blue-trans-donut/trans-donut{0:04d}.png'.format(x)))
+
+ani_donut_hover_animation = Animation.from_image_sequence(
+	ani_donut_hover_frames, 1.0/24, loop=True)
+
+ani_donut       = Sprite(ani_donut_animation,       x=100, y=100)
+ani_donut_hover = Sprite(ani_donut_hover_animation, x=100, y=100)
+
 ani_button = GameButton(ani_donut, hover=ani_donut_hover, sync_hover_state=True)
+
+# TODO Don't hardcode this
+donut_silhouette = SpriteSilhouette(donut, (255,255,150), x=500, y=100)
 
 @game_window.event
 def on_draw():
@@ -64,8 +61,7 @@ def on_mouse_motion(x, y, dx, dy):
 	ani_button.handle_mouse_motion(x, y)
 
 def update(dt):
-	button.update(dt)
-	ani_button.update(dt)
+	pass
 
 if __name__ == "__main__":
 	glEnable(GL_BLEND)
