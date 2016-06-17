6aac9bd535ac0b0ea03cece15b81d7bfeac1e81a
hundvalp <hundvalp@users.noreply.github.com>
2016-06-16 19:39:43 -0500

    Added support for animated silhouettes

--- sprite_silhouette.py
+++ sprite_silhouette.py
@@ -1,3 +1,4 @@
+from pyglet.image import Animation
 from pyglet.sprite import Sprite
 
 class SpriteSilhouette(Sprite):
@@ -12,32 +13,61 @@ class SpriteSilhouette(Sprite):
 		"""
 		Creates a silhouette of a Sprite.
 
+		This works for animated sprites as well.
+
 		Arguments:
 			sprite (:class:`pyglet.sprite.Sprite`): The Sprite to create a silhouette from.
 			color (tuple of int): RGB color value of the silhouette, values between 0 and 255.
 		"""
 		super(SpriteSilhouette, self).__init__(sprite.image, *args, **kwargs)
-		
-		# Create a copy of the original image
-		image_region = self.image.get_region(0, 0, self.image.width, self.image.height)
-		image_copy = image_region.get_image_data().texture.get_image_data()
 
-		# Number of pixels in the original image
-		pixels = self.image.width * self.image.height
+		# Cache of images which have already been created
+		self._image_cache = {}
 
-		# Get the original alpha data
-		alpha_data = bytes(image_copy.get_data("A", 1 * self.image.width))
-
-		# Prepare the new RGBA data
-		new_data = list(color) * pixels
-
-		# Insert the original alpha data into the new RGBA data as every 4th element
-		index = 3
-		for x in range(pixels):
-			new_data.insert(index, alpha_data[x])
-			index += 4
-
-		# Set the color of the new image
-		image_copy.set_data("RGBA", 4 * self.image.width, bytes(new_data))
+		if isinstance(sprite.image, Animation):
+			frames   = [self._create_silhouette_from(frame.image, color) for frame in sprite.image.frames]
+			duration = sprite.image.frames[0].duration
+			loop     = not sprite.image.frames[-1].duration is None
+			image_copy = Animation.from_image_sequence(frames, duration, loop=loop)
+		else:
+			image_copy = self._create_silhouette_from(sprite.image, color)
 
 		self.image = image_copy
+
+	def _create_silhouette_from(self, image, color):
+		"""
+		Creates a silhouette from the given :class:`pyglet.image.AbstractImage` object.
+
+		Arguments:
+			image (:class:`pyglet.image.AbstractImage`): The image to create a silhouette from.
+			color (tuple of int): RGB color value of the silhouette, values between 0 and 255.
+
+		Returns:
+			A new :class:`pyglet.image.AbstractImage` object created from the original.
+		"""
+		# Create a copy of the original image
+		image_copy = image.get_image_data()
+
+		# Number of pixels in the original image
+		pixels = image.width * image.height
+
+		# Get the original alpha data
+		alpha_data = image_copy.get_data("A", image.width)
+		alpha_hash = hash(tuple(alpha_data))
+
+		if alpha_hash in self._image_cache:
+			return self._image_cache[alpha_hash]
+
+		# Prepare the new RGBA data
+		new_data = bytearray()
+		for a in alpha_data:
+			new_data.extend(color)
+			new_data.append(a)
+
+		# Set the color of the new image
+		image_copy.set_data("RGBA", 4 * image.width, bytes(new_data))
+
+		# Cache this image
+		self._image_cache[alpha_hash] = image_copy
+
+		return image_copy
